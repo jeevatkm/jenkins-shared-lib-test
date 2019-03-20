9ea30eb3d45e4b86cc25351b81482548c1bbf49e
Jeevanandam M <jeeva@myjeeva.com>
2019-03-20 00:04:05 -0700

    wip

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -2,10 +2,14 @@
 @Grab('org.apache.commons:commons-lang3:3.8.1')
 
 import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.mutable.MutableObject
 
 def call(Map pipeParams) {
     println 'customPipeline called successfully'
 
     println StringUtils.isAllUpperCase("ABC")
+
+    def mutableObject = new MutableObject("Initial value");
+    println mutableObject
 
 }
