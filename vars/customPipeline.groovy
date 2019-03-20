becf898a10cb4fc9efd293aca3eec53a761f792a
Jeevanandam M <jeeva@myjeeva.com>
2019-03-20 00:18:13 -0700

    wip

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -1,8 +1,12 @@
 
 @Grab('org.apache.commons:commons-lang3:3.8.1')
+@Grab('software.amazon.awssdk:cloudformation:2.5.11')
 
 import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.mutable.MutableObject
+
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.cloudformation.CloudFormationClient
 
 def call(Map pipeParams) {
     println 'customPipeline called successfully'
@@ -12,4 +16,6 @@ def call(Map pipeParams) {
     def mutableObject = new MutableObject("Initial value");
     println mutableObject
 
+    def client = CloudFormationClient.bulider().region(Region.of('us-west-1')).build()
+    println client
 }
