b254dea1b29a57174f8a877c84f0e45f4bfef941
Jeevanandam M <jeeva@myjeeva.com>
2019-03-20 00:53:25 -0700

    wip

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -1,13 +1,13 @@
 
 @Grab('org.apache.commons:commons-lang3:3.8.1')
-// @Grab('software.amazon.awssdk:cloudformation:2.5.11')
+@Grab('software.amazon.awssdk:cloudformation:2.5.11')
 @Grab('com.amazonaws:aws-java-sdk:1.11.205')
 
 import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.mutable.MutableObject
 
-// import software.amazon.awssdk.regions.Region
-// import software.amazon.awssdk.cloudformation.CloudFormationClient
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.cloudformation.CloudFormationClient
 
 import com.amazonaws.AmazonClientException
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
@@ -23,8 +23,8 @@ def call(Map pipeParams) {
     def mutableObject = new MutableObject("Initial value");
     println mutableObject
 
-    // def client = CloudFormationClient.bulider().region(Region.of('us-west-1')).build()
-    // println client
+    def client = CloudFormationClient.builder().region(Region.of('us-west-1')).build()
+    println client
 
     ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
         try {
